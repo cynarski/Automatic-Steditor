bc637f9510ed745181f31da60f3e45e42046b6bb
cynarski <cynarski60@gmail.com>
2023-11-08 09:10:23 +0100

    add style

--- app.js
+++ app.js
@@ -4,15 +4,11 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-let routingControl;
+let routingControls = [];
 
 function showRoute() {
     const startCity = document.getElementById('startCity').value;
     const endCity = document.getElementById('endCity').value;
-
-    if (routingControl) {
-        map.removeControl(routingControl);
-    }
 
     fetch(`https://nominatim.openstreetmap.org/search?city=${startCity}&format=json&limit=1`)
         .then(response => response.json())
@@ -26,13 +22,34 @@ function showRoute() {
                     const endLat = parseFloat(dataEnd[0].lat);
                     const endLon = parseFloat(dataEnd[0].lon);
 
-                    routingControl = L.Routing.control({
+                    const routingControl = L.Routing.control({
                         waypoints: [
                             L.latLng(startLat, startLon),
                             L.latLng(endLat, endLon)
                         ],
                         routeWhileDragging: true
                     }).addTo(map);
+
+                    routingControl.on('routesfound', function(e) {
+                        const routes = e.routes;
+                        const summary = routes[0].summary;
+                        const distance = (summary.totalDistance / 1000).toFixed(2);
+
+                        // Dodajemy informacje o trasie do listy
+                        const routeInfoItem = document.createElement('li');
+                        routeInfoItem.innerText = `${startCity} do ${endCity}: ${distance} km`;
+                        document.getElementById('route-info-list').appendChild(routeInfoItem);
+                    });
+
+                    routingControls.push(routingControl);
                 });
         });
+}
+
+function clearRoutes() {
+    for (let i = 0; i < routingControls.length; i++) {
+        map.removeControl(routingControls[i]);
+    }
+    routingControls = [];
+    document.getElementById('route-info-list').innerHTML = ''; // Czyszczenie listy
 }

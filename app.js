0221001f521a9813e902b080a2df4bc436450dfa
michalcynarski <cynarski60@gmail.com>
2023-11-15 01:07:54 +0100

    big chnges in first web

--- app.js
+++ app.js
@@ -55,33 +55,24 @@
 // }
 
 function saveCity() {
-    const city = document.getElementById('city').value;
-    const product = document.getElementById('productSelect').value;
+    const city = document.getElementById('citySelect').value;
+    const productSelect = document.getElementById('productSelect');
+    const weight = document.getElementById('productWeight').value;
 
-    if (!city || !product) {
-        alert("Proszę wybrać miasto i produkt.");
+    if (!city || productSelect.selectedOptions.length === 0 || !weight) {
+        alert("Proszę wybrać miasto, przynajmniej jeden produkt i wpisać wagę.");
         return;
     }
 
+    const products = Array.from(productSelect.selectedOptions).map(option => option.value);
+
     let selections = JSON.parse(localStorage.getItem('selections')) || [];
-    selections.push({ city, product });
+    selections.push({ city, products, weight });
     localStorage.setItem('selections', JSON.stringify(selections));
 
     updateDisplay();
 }
 
-function showProductImage() {
-    var select = document.getElementById('productSelect');
-    var image = document.getElementById('productImage');
-    var selectedValue = select.value;
-
-    if (selectedValue) {
-        image.src = 'images/' + selectedValue;
-        image.style.display = 'block';
-    } else {
-        image.style.display = 'none';
-    }
-}
 
 function updateDisplay() {
     const selections = JSON.parse(localStorage.getItem('selections')) || [];
@@ -93,9 +84,11 @@ function updateDisplay() {
         let row = tableBody.insertRow();
         let cellCity = row.insertCell(0);
         let cellProduct = row.insertCell(1);
+        let cellWeight = row.insertCell(2);
 
         cellCity.textContent = selection.city;
-        cellProduct.textContent = selection.product;
+        cellProduct.textContent = selection.products.join(', '); // Łączenie produktów w jeden ciąg tekstowy
+        cellWeight.textContent = selection.weight + ' t';
     });
 }
 
@@ -103,6 +96,17 @@ function clearSelection() {
     localStorage.removeItem('selections');
     updateDisplay();
     document.getElementById('productImage').style.display = 'none';
+    if (window.clearMarkers)
+    {
+        window.clearMarkers();
+    }
+
+}
+function clearEverything() {
+    clearSelection(); // funkcja czyszcząca tabelę
+    if (window.clearMarkers) {
+        window.clearMarkers(); // funkcja czyszcząca markery na mapie
+    }
 }
 
 updateDisplay();

38bc524e9b278cea70de696c683a9ef87258f49b
cynarski <cynarski60@gmail.com>
2023-12-13 08:49:32 +0100

    Update

--- app.py
+++ app.py
@@ -23,6 +23,8 @@ def filter_cities():
         lat, lon = get_lat_lon_from_city_name(city_name)
         if lat and lon:
             cities_data.append({"city": city_name, "lat": lat, "lon": lon})
+    print(cities_data)
+    print(jsonify(cities_data))
     return jsonify(cities_data)
 
 def get_lat_lon_from_city_name(city_name):

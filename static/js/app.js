452cef9328292d13c2362ee6926346c1581ef663
cynarski <cynarski60@gmail.com>
2023-12-28 20:16:30 +0100

    add parameters to webside and change js structure

--- static/js/app.js
+++ static/js/app.js
@@ -1,41 +1,50 @@
-let selections = []; // Teraz dane są przechowywane w pamięci, a nie w localStorage
-
-function saveCity() {
-    const city = document.getElementById('citySelect').value;
-    const productSelect = document.getElementById('productSelect');
-    const weight = document.getElementById('productWeight').value;
-    const deadline = document.getElementById('deliveryDeadline').value;
-
-    if (!city || productSelect.selectedOptions.length === 0 || !weight || !deadline) {
-        alert("Proszę wybrać miasto, przynajmniej jeden produkt, wpisać wagę oraz ustawić termin dostawy.");
-        return;
-    }
-
-    const products = Array.from(productSelect.selectedOptions).map(option => option.value);
-    selections.push({ city, products, weight, deadline });
-    updateDisplay();
-
-    // Zapisywanie danych w localStorage
-    localStorage.setItem('selections', JSON.stringify(selections));
-}
-
-function updateDisplay() {
-    const tableBody = document.getElementById('infoTable').getElementsByTagName('tbody')[0];
-    tableBody.innerHTML = '';
-
-    selections.forEach(selection => {
-        let row = tableBody.insertRow();
-        let cellCity = row.insertCell(0);
-        let cellProduct = row.insertCell(1);
-        let cellWeight = row.insertCell(2);
-        let cellDeadline = row.insertCell(3);
-
-        cellCity.textContent = selection.city;
-        cellProduct.textContent = selection.products.join(', ');
-        cellWeight.textContent = selection.weight + ' t';
-        cellDeadline.textContent = selection.deadline + ' days';
-    });
-}
+// let selections = []; // Teraz dane są przechowywane w pamięci, a nie w localStorage
+//
+// function saveCity() {
+//     const city = document.getElementById('citySelect').value;
+//     const productSelect = document.getElementById('productSelect');
+//     const weight = document.getElementById('productWeight').value;
+//     const deadline = document.getElementById('deliveryDeadline').value;
+//
+//     if (!city || productSelect.selectedOptions.length === 0 || !weight || !deadline) {
+//         alert("Proszę wybrać miasto, przynajmniej jeden produkt, wpisać wagę oraz ustawić termin dostawy.");
+//         return;
+//     }
+//
+//     const products = Array.from(productSelect.selectedOptions).map(option => option.value);
+//     selections.push({ city, products, weight, deadline });
+//     updateDisplay();
+//
+//     // Zapisywanie danych w localStorage
+//     localStorage.setItem('selections', JSON.stringify(selections));
+// }
+//
+// function updateDisplay() {
+//     const tableBody = document.getElementById('infoTable').getElementsByTagName('tbody')[0];
+//     tableBody.innerHTML = '';
+//
+//     selections.forEach(selection => {
+//         let row = tableBody.insertRow();
+//         let cellCity = row.insertCell(0);
+//         let cellProduct = row.insertCell(1);
+//         let cellWeight = row.insertCell(2);
+//         let cellDeadline = row.insertCell(3);
+//
+//         cellCity.textContent = selection.city;
+//         cellProduct.textContent = selection.products.join(', ');
+//         cellWeight.textContent = selection.weight + ' t';
+//         cellDeadline.textContent = selection.deadline + ' days';
+//     });
+// }
+//
+// document.addEventListener('DOMContentLoaded', function() {
+//     // Wczytywanie danych z localStorage i aktualizacja wyświetlania tabeli
+//     const storedSelections = localStorage.getItem('selections');
+//     if (storedSelections) {
+//         selections = JSON.parse(storedSelections);
+//         updateDisplay();
+//     }
+// });
 
 function clearSelection() {
     selections = [];
@@ -51,14 +60,7 @@ function clearEverything() {
     clearMarkers();
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    // Wczytywanie danych z localStorage i aktualizacja wyświetlania tabeli
-    const storedSelections = localStorage.getItem('selections');
-    if (storedSelections) {
-        selections = JSON.parse(storedSelections);
-        updateDisplay();
-    }
-});
+
 
 function saveCitiesToLocalStorage() {
     const selectedCities = selections.map(selection => selection.city);
@@ -73,7 +75,7 @@ document.getElementById('goToMapButton').addEventListener('click', function() {
     setTimeout(function() {
         saveCitiesToLocalStorage();
         window.location.href = '/map';
-    }, 5000)
+    }, 2500)
     this.disabled = true;
 });
 
